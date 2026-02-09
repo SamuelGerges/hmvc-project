2ae5d51447f941d1411b54c826d83cbb46226d61
SamuelGerges <samoulgerges00@gmail.com>
2026-02-09 14:08:34 +0200

    second commit

--- Erp/HR/routes/web.php
+++ Erp/HR/routes/web.php
@@ -1,10 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+use Erp\HR\App\Http\Controllers\Admin\HRController;
 
-Route::middleware(['web'])
-    ->prefix('erp/h-r')
-    ->name('erp.h_r.')
-    ->group(function () {
-        Route::get('/', [\Erp\HR\App\Http\Controllers\HRController::class, 'index'])->name('index');
-    });
+//Route::group(
+//    [
+//        'prefix' => LaravelLocalization::setLocale(),
+//        'middleware' => [
+//            'auth:web',
+//            '2faMiddelware',
+//            'web',
+//            'localeSessionRedirect',
+//            'localizationRedirect',
+//            'localeViewPath'
+//        ]
+//    ],
+//    function () {
+//        Route::resource('h-r', HRController::class);
+//
+//
+//
+//    }
+//);
+
+Route::resource('h-r', HRController::class);
